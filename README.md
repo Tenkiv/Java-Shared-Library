37705c253776b6767de95fc31f2590ac54ffa743
ejberry <ejberry@tenkiv.com>
2016-02-18 10:40:01 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,59 @@
+Tekdaqc-Java-Library
+====================
+
+Java library to connect to, control and read data from Tekdaqcs.
+
+Join [Intelligent Automation, Computer Interface, & DAQ Community](https://plus.google.com/u/0/communities/109351353187504550254) on [![DAQ Community on Google Plus](https://ssl.gstatic.com/images/icons/gplus-16.png)](https://plus.google.com/u/0/communities/109351353187504550254) to stay up-to-date on the latest news.
+
+## Using the Tekdaqc Java Library
+
+### Dependencies
+* [Apache Commons Net Library](http://commons.apache.org/proper/commons-net/) _NOTE: This is currently included in the repository_
+
+### Setup
+
+1. Clone or download a copy of the Tekdaqc Android Library source code.
+2. Import the Tekdaqc Java Library project into Eclipse.
+3. In your project's build path settings, add a project reference to Tekdaqc Java Library.
+
+## More Information
+
+### Other Tekdaqc GIT Repositories
+* [Tekdaqc Android Library](https://github.com/Tenkiv/Tekdaqc-Android-Library)
+* [Tekdaqc Android Manager](https://github.com/Tenkiv/Tekdaqc-Android-Manager)
+* [Tekdaqc Firmware](https://github.com/Tenkiv/Tekdaqc-Firmware)
+
+### Tekdaqc Manual
+* Download the [Tekdaqc Manual here](http://www.tenkiv.com/tekdaqc_manual_pdf_v3.pdf)
+
+### Other Links
+* [Tenkiv Webpage](http://www.tenkiv.com/)
+* [Intelligent Automation, Computer Interface, & DAQ Community](https://plus.google.com/u/0/communities/109351353187504550254) on [![DAQ Community on Google Plus](https://ssl.gstatic.com/images/icons/gplus-16.png)](https://plus.google.com/u/0/communities/109351353187504550254)
+
+## Contributing
+
+Please see our [contribution guidelines](https://github.com/Tenkiv/Tekdaqc-Android-Library/blob/master/CONTRIBUTING.md) if you have issues or code contributions.
+
+### Contributors
+#### Tenkiv, Inc.
+* [Jared Woolston](https://github.com/jwoolston)
+* [Ellis Berry](https://github.com/ejberry)
+
+#### Third Party
+* [Ian Thomas at ToxicBakery](https://github.com/ToxicBakery)
+
+## License
+
+    Copyright 2013 Tenkiv, Inc.
+    
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    
+    http://www.apache.org/licenses/LICENSE-2.0
+    
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ src/main/java/com/tenkiv/tekdaqc/utility/DigitalOutputUtilities.java
@@ -0,0 +1,84 @@
+package com.tenkiv.tekdaqc.utility;
+
+/**
+ * Utility class to convert binary string to their hexadecimal representation.
+ */
+public class DigitalOutputUtilities {
+
+
+    /**
+     * Converts a binary string to hexadecimal. Hexadecimal is used in communication with the tekdaqc
+     * for setting and reading the digital outputs.
+     *
+     * @param binaryString The string to be converted.
+     * @return String in hexadecimal
+     */
+    public static String hexConversion(String binaryString) {
+
+        StringBuilder hexBuilder = new StringBuilder();
+
+        while (binaryString.length() > 0) {
+
+            int decimal = Integer.parseInt(binaryString.substring(0, 4), 2);
+            String hexStr = Integer.toString(decimal, 16);
+
+            hexBuilder.append(hexStr);
+
+            binaryString = binaryString.substring(4, binaryString.length());
+        }
+        return hexBuilder.toString();
+    }
+
+    /**
+     * Converts a boolean array to hexadecimal. Hexadecimal is used in communication with the tekdaqc
+     * for setting and reading the digital outputs.
+     *
+     * @param digitalOutputData The string to be converted.
+     * @return String in hexadecimal
+     */
+    public static String boolArrayConversion(final boolean[] digitalOutputData) {
+
+        StringBuilder binStringBuilder = new StringBuilder();
+
+        for (boolean digOut : digitalOutputData) {
+
+            if (digOut) {
+                binStringBuilder.append("1");
+
+            } else {
+                binStringBuilder.append("0");
+            }
+        }
+        return (hexConversion(binStringBuilder.toString()));
+    }
+
+    /**
+     * Converts a hexadecimal {@link String} to a binary string.
+     *
+     * @param hex The hex {@link String} to be converted.
+     * @return The Binary string of the hex string.
+     */
+    public static String hex_to_binary(final String hex) {
+        String hex_char, bin_char, binary;
+        binary = "";
+        int len = hex.length() / 2;
+        for (int i = 0; i < len; i++) {
+            hex_char = hex.substring(2 * i, 2 * i + 2);
+            int conv_int = Integer.parseInt(hex_char, 16);
+            bin_char = Integer.toBinaryString(conv_int);
+            bin_char = zero_pad_bin_char(bin_char);
+            if (i == 0) binary = bin_char;
+            else binary = binary + bin_char;
+            //out.printf("%s %s\n", hex_char,bin_char);
+        }
+        return binary;
+    }
+
+    public static String zero_pad_bin_char(final String bin_char) {
+        int len = bin_char.length();
+        if (len == 8) return bin_char;
+        String zero_pad = "0";
+        for (int i = 1; i < 8 - len; i++) zero_pad = zero_pad + "0";
+        return zero_pad + bin_char;
+    }
+}
