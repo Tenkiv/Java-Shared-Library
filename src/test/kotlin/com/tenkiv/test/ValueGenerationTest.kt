92d3cae867680aa43b07d8a4fc04cadf98c151ed
Zack Juhasz <zjuhasz@tenkiv.com>
2017-09-15 17:53:04 -0700

    Undid test break.

--- src/test/kotlin/com/tenkiv/test/ValueGenerationTest.kt
+++ src/test/kotlin/com/tenkiv/test/ValueGenerationTest.kt
@@ -2,20 +2,18 @@ package com.tenkiv.test
 
 import com.tenkiv.tekdaqc.hardware.ATekdaqc
 import com.tenkiv.tekdaqc.hardware.CommandBuilder
-import io.kotlintest.matchers.be
 import io.kotlintest.specs.StringSpec
 
 /**
  * Created by tenkiv on 4/29/17.
  */
 
-class ValueGenerationTest: StringSpec(){
+class ValueGenerationTest : StringSpec() {
 
     val SET_ANALOG_INPUT_SCALE = "SET_ANALOG_INPUT_SCALE --SCALE=ANALOG_SCALE_5V\r"
     val ADD_ANALOG_INPUT = "ADD_ANALOG_INPUT --INPUT=0 --RATE=5 --GAIN=2 --BUFFER=ON\r"
 
-    init{
-        throw Exception()
+    init {
         "Generating Values"{
 
             val aiScale = CommandBuilder.setAnalogInputScale(ATekdaqc.AnalogScale.ANALOG_SCALE_5V)
